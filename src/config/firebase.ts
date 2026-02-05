edb30c777d845850413bb36e57e39548a91562c5
Manuel Galvez <galvez.alejo@gmail.com>
2026-02-04 21:22:17 -0500

    Removing kyes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtRNqep3BD1nI1gDhOBaFT6ps-txWsiNM",
-    authDomain: "tennis-driveapp.firebaseapp.com",
-    projectId: "tennis-driveapp",
-    storageBucket: "tennis-driveapp.firebasestorage.app",
-    messagingSenderId: "109946831008",
-    appId: "1:109946831008:web:0999f7a234ad3ca44649c5",
-    measurementId: "G-KR022E89PB"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
